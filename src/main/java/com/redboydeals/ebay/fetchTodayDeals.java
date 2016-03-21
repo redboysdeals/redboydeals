230a5e593e234b59e1e31e5e68545ac7e3de465e
saramasam <Admin@10.11.107.9>
2016-03-21 15:08:28 -0400

    first checking

---
+++ src/main/java/com/redboydeals/ebay/fetchTodayDeals.java
@@ -0,0 +1,22 @@
+/**
+ * 
+ */
+package com.redboydeals.ebay;
+
+/**
+ * @author Sankar Ramasamy
+ *
+ * @date Mar 21, 2016
+ * @filename fetchTodayDeals.java
+ */
+public class fetchTodayDeals {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hi");
+	}
+
+}
